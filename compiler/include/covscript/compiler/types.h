d8c36d3b4abc1d3930486aa9ae861f9f6e08891e
imkiva <libkernelpanic@gmail.com>
2019-09-19 17:14:02 +0800

    shared: move vm_type.hpp to shared

---
+++ compiler/include/covscript/compiler/types.h
@@ -0,0 +1,17 @@
+//
+// Created by kiva on 2019/9/19.
+//
+#pragma once
+
+#include <covscript/shared/vm_type.hpp>
+
+namespace cs {
+    namespace compiler {
+        using VmInt = cs::builtin_types::vm_int;
+        using VmFloat = cs::builtin_types::vm_float;
+        using VmBool = cs::builtin_types::vm_bool;
+        using VmChar = cs::builtin_types::vm_char;
+        using VmString = cs::builtin_types::vm_string;
+    }
+}
+
