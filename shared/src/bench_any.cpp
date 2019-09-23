6cb8c7e1a14ed1aaceb19e38fff1425c89a4d31f
Michael Lee <mikecovlee@163.com>
2019-09-23 20:32:20 +0800

    Update bench_any.cpp

--- shared/src/bench_any.cpp
+++ shared/src/bench_any.cpp
@@ -9,6 +9,12 @@
 
 int test_epoch = 10000000;
 
+struct test_large_data {
+	std::string str;
+	unsigned char alignment[32];
+	test_large_data(const char* s) : str(s) {}
+};
+
 int main()
 {
 	std::cout << "Size of std::any: " << sizeof(std::any) << std::endl;
@@ -40,29 +46,29 @@ int main()
 		}
 	}) << std::endl;
 
-	std::cout << "[Big Data] std::any copying: " << cs::utility::timer::measure([]() {
-		std::any a(std::string("Hello"));
+	std::cout << "[Large Data] std::any copying: " << cs::utility::timer::measure([]() {
+		std::any a(test_large_data("Hello"));
 		for (int i = 0; i < test_epoch; ++i)
 			std::any b(a);
 	}) << std::endl;
-	std::cout << "[Big Data] cov::any copying: " << cs::utility::timer::measure([]() {
-		cs::runtime::any a(std::string("Hello"));
+	std::cout << "[Large Data] cov::any copying: " << cs::utility::timer::measure([]() {
+		cs::runtime::any a(test_large_data("Hello"));
 		for (int i = 0; i < test_epoch; ++i)
 			cs::runtime::any b(a);
 	}) << std::endl;
 
-	std::cout << "[Big Data] std::any instancing: " << cs::utility::timer::measure([]() {
-		std::any a(std::string("Hello"));
+	std::cout << "[Large Data] std::any instancing: " << cs::utility::timer::measure([]() {
+		std::any a(test_large_data("Hello"));
 		for (int i = 0; i < test_epoch; ++i) {
-			std::any_cast<std::string &>(a).push_back('c');
-			std::any_cast<std::string &>(a).pop_back();
+			std::any_cast<test_large_data &>(a).str.push_back('c');
+			std::any_cast<test_large_data &>(a).str.pop_back();
 		}
 	}) << std::endl;
-	std::cout << "[Big Data] cov::any instancing: " << cs::utility::timer::measure([]() {
-		cs::runtime::any b(std::string("Hello"));
+	std::cout << "[Large Data] cov::any instancing: " << cs::utility::timer::measure([]() {
+		cs::runtime::any b(test_large_data("Hello"));
 		for (int i = 0; i < test_epoch; ++i) {
-			b.get<std::string>().push_back('c');
-			b.get<std::string>().pop_back();
+			b.get<test_large_data>().str.push_back('c');
+			b.get<test_large_data>().str.pop_back();
 		}
 	}) << std::endl;
 
