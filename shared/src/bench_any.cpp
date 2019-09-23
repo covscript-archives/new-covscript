7814cd17ec37dbb1d0b7c1d73b9fa888cb95ad07
Michael Lee <mikecovlee@163.com>
2019-09-23 19:31:54 +0800

    Update bench_any.cpp

--- shared/src/bench_any.cpp
+++ shared/src/bench_any.cpp
@@ -14,25 +14,51 @@ int main()
 	std::cout << "Size of std::any: " << sizeof(std::any) << std::endl;
 	std::cout << "Size of cov::any: " << sizeof(cs::runtime::any) << std::endl;
 
-	std::cout << "std::any copying: " << cs::utility::timer::measure([]() {
+	std::cout << "[Small Data] std::any copying: " << cs::utility::timer::measure([]() {
+		std::any a(10);
+		for (int i = 0; i < test_epoch; ++i)
+			std::any b(a);
+	}) << std::endl;
+	std::cout << "[Small Data] cov::any copying: " << cs::utility::timer::measure([]() {
+		cs::runtime::any a(10);
+		for (int i = 0; i < test_epoch; ++i)
+			cs::runtime::any b(a);
+	}) << std::endl;
+
+	std::cout << "[Small Data] std::any instancing: " << cs::utility::timer::measure([]() {
+		std::any a(10);
+		for (int i = 0; i < test_epoch; ++i) {
+			++std::any_cast<int &>(a);
+			--std::any_cast<int &>(a);
+		}
+	}) << std::endl;
+	std::cout << "[Small Data] cov::any instancing: " << cs::utility::timer::measure([]() {
+		cs::runtime::any b(10);
+		for (int i = 0; i < test_epoch; ++i) {
+			++b.get<int>();
+			--b.get<int>();
+		}
+	}) << std::endl;
+
+	std::cout << "[Big Data] std::any copying: " << cs::utility::timer::measure([]() {
 		std::any a(std::string("Hello"));
 		for (int i = 0; i < test_epoch; ++i)
 			std::any b(a);
 	}) << std::endl;
-	std::cout << "cov::any copying: " << cs::utility::timer::measure([]() {
+	std::cout << "[Big Data] cov::any copying: " << cs::utility::timer::measure([]() {
 		cs::runtime::any a(std::string("Hello"));
 		for (int i = 0; i < test_epoch; ++i)
 			cs::runtime::any b(a);
 	}) << std::endl;
 
-	std::cout << "std::any instancing: " << cs::utility::timer::measure([]() {
+	std::cout << "[Big Data] std::any instancing: " << cs::utility::timer::measure([]() {
 		std::any a(std::string("Hello"));
 		for (int i = 0; i < test_epoch; ++i) {
 			std::any_cast<std::string &>(a).push_back('c');
 			std::any_cast<std::string &>(a).pop_back();
 		}
 	}) << std::endl;
-	std::cout << "cov::any instancing: " << cs::utility::timer::measure([]() {
+	std::cout << "[Big Data] cov::any instancing: " << cs::utility::timer::measure([]() {
 		cs::runtime::any b(std::string("Hello"));
 		for (int i = 0; i < test_epoch; ++i) {
 			b.get<std::string>().push_back('c');
