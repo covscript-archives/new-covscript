55ced91d9c78b30670404e94cc4175ce32cfe541
Michael Lee <mikecovlee@163.com>
2019-09-26 11:09:35 +0800

    Update vm_instance.hpp

--- runtime/include/covscript/runtime/vm_instance.hpp
+++ runtime/include/covscript/runtime/vm_instance.hpp
@@ -1,9 +1,52 @@
 #pragma once
 
 #include <covscript/shared/memory.hpp>
+#include <covscript/shared/any.hpp>
 
 namespace cs {
+    enum class instrument_types {
+        // Expression
+        expr,
+        // Direct Jump
+        jmp,
+        // Conditional Jump
+        jcond,
+        // Push Stack Frame
+        push,
+        // Pop Stack Frame
+        pop,
+        // Call Function
+        call,
+        // Return Function
+        ret,
+        // Join Stackfull Coroutine
+        joins,
+        // Join Stackless Coroutine
+        joinl,
+        // Trigger Coroutine Dispatch
+        yield,
+        // Register Exception Handler
+        regeh,
+        // Throw Exception
+        thex,
+        // Variable Declaration
+        vdecl,
+        // Null
+        null
+    };
+    class vm_instrument {
+
+    };
     class vm_instance final {
+        class stack_frame_base {
+
+        };
+        class stack_frame_range {
+
+        };
+        class stack_frame_func {
+
+        };
         runtime::stack_type<runtime::stack_type<runtime::any>> m_stack;
     };
 }
